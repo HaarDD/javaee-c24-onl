64553c4b6251619d830c8c5b3e404c76a517bfc1
HaarDD <maxgalitskiy@gmail.com>
2023-10-17 01:23:13 +0300

    refactor: regex parse url queries

--- src/main/java/by/teachmeskills/lesson21/util/HttpUtils.java
+++ src/main/java/by/teachmeskills/lesson21/util/HttpUtils.java
@@ -2,17 +2,22 @@ package by.teachmeskills.lesson21.util;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class HttpUtils {
 
-    public static Map<String, String> parseUriQueryParams(String queryParam){
+    public static final Pattern REGEX_URL_QUERIES = Pattern.compile("(?<name>[^?=&]+)(?>=(?<value>[^&]*))?");
+
+    public static Map<String, String> parseUriQueryParams(String queryParam) {
         Map<String, String> paramMap = new HashMap<>();
-        String[] params = queryParam.split("&");
-        for (String param : params) {
-            String[] split = param.split("=");
-            String paramName = split[0];
-            String paramValue = split[1];
-            paramMap.put(paramName, paramValue);
+        Matcher matcher = REGEX_URL_QUERIES.matcher(queryParam);
+        while (matcher.find()) {
+            String name = matcher.group("name");
+            String value = matcher.group("value");
+            if (!name.isEmpty() && !value.isEmpty()) {
+                paramMap.put(name, value);
+            }
         }
         return paramMap;
     }
