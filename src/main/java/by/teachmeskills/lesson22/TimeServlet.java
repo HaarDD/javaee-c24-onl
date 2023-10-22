022e169055140a9964b2d0d56844e09e8cac87fd
HaarDD <maxgalitskiy@gmail.com>
2023-10-23 01:15:17 +0300

    feat: add time servlet (/minsk = time in Minsk, etc.)

---
+++ src/main/java/by/teachmeskills/lesson22/TimeServlet.java
@@ -0,0 +1,46 @@
+package by.teachmeskills.lesson22;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
+
+@WebServlet(urlPatterns = {"/minsk", "/washington", "/beijing"}, loadOnStartup = 1)
+public class TimeServlet extends HttpServlet {
+    Map<String, ZoneId> cityTimeZoneMap = new HashMap<>();
+
+    {
+        cityTimeZoneMap.put("minsk", TimeZone.getTimeZone("Europe/Minsk").toZoneId());
+        cityTimeZoneMap.put("washington", TimeZone.getTimeZone("America/New_York").toZoneId());
+        cityTimeZoneMap.put("beijing", TimeZone.getTimeZone("Asia/Shanghai").toZoneId());
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String requestURI = request.getRequestURI();
+        PrintWriter writer = response.getWriter();
+        getTimeStrByCityPath(cityTimeZoneMap, requestURI, writer);
+    }
+
+    private void getTimeStrByCityPath(Map<String, ZoneId> cityTimeZoneMap, String requestURI, PrintWriter writer) {
+        cityTimeZoneMap.forEach((k, v) -> {
+            if (requestURI.contains(k)) {
+                String time = ZonedDateTime.now(v).toLocalDateTime().format(DateTimeFormatter.ISO_DATE)
+                        + " " + ZonedDateTime.now(v).toLocalDateTime().truncatedTo(ChronoUnit.SECONDS).format(DateTimeFormatter.ISO_LOCAL_TIME);
+                writer.println("It is now " + time + " in the " + k + ".");
+            }
+        });
+    }
+
+}
