2011b57017b36864bc1181fd5316a130e61ca278
HaarDD <maxgalitskiy@gmail.com>
2023-11-13 01:18:37 +0300

    fix: move read json & get list from session attribute functions to DataUtils class

---
+++ src/main/java/by/teachmeskills/lesson26/services/DataUtils.java
@@ -0,0 +1,35 @@
+package by.teachmeskills.lesson26.services;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+
+public class DataUtils<T> {
+
+    public static String readJsonFromRequest(HttpServletRequest request) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(request.getInputStream()))) {
+            return reader.readLine();
+        }
+    }
+
+    public static <T> List<T> getListFromAttributeDataSession(HttpSession session, String attributeName, Class<T> attributeListClass) {
+        Object attributeValue = session.getAttribute(attributeName);
+
+        if (attributeValue instanceof List<?> rawList) {
+
+            for (Object item : rawList) {
+                if (!attributeListClass.isInstance(item)) {
+                    return null;
+                }
+            }
+
+            return (List<T>) rawList;
+        }
+
+        return null;
+    }
+
+}
