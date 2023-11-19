842ed2a1138009259b5913ec81298909a8069382
HaarDD <maxgalitskiy@gmail.com>
2023-11-19 23:41:16 +0300

    feat: O - Open Closed Principle good/bad examples

---
+++ servlet/src/main/java/by/teachmeskills/lesson29/ocp/bad/service/NotificationService.java
@@ -0,0 +1,18 @@
+package by.teachmeskills.lesson29.ocp.bad.service;
+
+import by.teachmeskills.lesson29.ocp.bad.dto.Order;
+
+public class NotificationService {
+
+    public static final int NOTIFICATION_TYPE_EMAIL = 1;
+    public static final int NOTIFICATION_TYPE_SMS = 2;
+
+    public void sendOrderClientNotification(Order order, int notificationType) {
+        if (notificationType == NOTIFICATION_TYPE_EMAIL) {
+            System.out.println("Письмо отправлено: " + order.getClient().getEmail() + " " + order);
+        } else if (notificationType == NOTIFICATION_TYPE_SMS) {
+            System.out.println("СМС отправлено: " + order.getClient().getPhoneNumber() + " " + order);
+        }
+    }
+
+}
---
+++ servlet/src/main/java/by/teachmeskills/lesson29/ocp/good/service/notification/SmsNotification.java
@@ -0,0 +1,8 @@
+package by.teachmeskills.lesson29.ocp.good.service.notification;
+
+public class SmsNotification implements Notification {
+    @Override
+    public void sendNotification(String address, String message) {
+        System.out.println("СМС отправлено: " + address + " " + message);
+    }
+}
