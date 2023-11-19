ad8f71cfd2228acb8722377536151e2d18728415
HaarDD <maxgalitskiy@gmail.com>
2023-11-19 06:55:50 +0300

    fix: moved setting servlet context attributes to web listener

--- src/main/java/by/teachmeskills/lesson26/PrintRequestServlet.java
+++ src/main/java/by/teachmeskills/lesson26/PrintRequestServlet.java
@@ -6,17 +6,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
-import static by.teachmeskills.lesson26.SaveRequestServlet.SERVICE_LIST;
-import static by.teachmeskills.lesson26.dto.RepairRequest.REPAIR_REQUEST_FIELDS_CYR_NAMES;
-
 @WebServlet(urlPatterns = "/print-request")
 public class PrintRequestServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getServletContext().setAttribute("fieldsTranslator", REPAIR_REQUEST_FIELDS_CYR_NAMES);
-        getServletContext().setAttribute("listElementsTranslator", SERVICE_LIST);
         request.getRequestDispatcher("WEB-INF/lesson26/print-request.jsp").forward(request, response);
     }
 }
