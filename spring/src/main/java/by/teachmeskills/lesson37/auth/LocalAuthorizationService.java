49d7f76a067e477ff488d404377c1e7096dde0d9
HaarDD <maxgalitskiy@gmail.com>
2023-12-20 14:09:45 +0300

    feat: add local authorization service (authorization service implementation)

---
+++ spring/src/main/java/by/teachmeskills/lesson37/auth/LocalAuthorizationService.java
@@ -0,0 +1,46 @@
+package by.teachmeskills.lesson37.auth;
+
+import by.teachmeskills.lesson37.auth.session.Session;
+import by.teachmeskills.lesson37.auth.session.FileServerSessionManager;
+import by.teachmeskills.lesson37.auth.session.SessionService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LocalAuthorizationService implements AuthorizationService, RegistrationService {
+
+    private final FileServerSessionManager sessionManager;
+
+    @Autowired
+    public LocalAuthorizationService(FileServerSessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Override
+    public boolean login(String username, String password) {
+        Session session = new Session(SessionService.generateSessionId(), username, this);
+        sessionManager.addSession(session);
+        log.info("{}: Пользователь вошел: {}", this, username);
+        return isUserAuthenticated(username);
+    }
+
+    @Override
+    public void logout(String username) {
+        sessionManager.removeSession(sessionManager.getSessionByUsername(username));
+        log.info("{}: Пользователь вышел: {}", this, username);
+    }
+
+    @Override
+    public boolean isUserAuthenticated(String username) {
+        boolean isUserAuthenticated = sessionManager.isSessionExistByUsername(username);
+        log.info("{}: Статус авторизации пользователя: {}, {}", this, username, isUserAuthenticated);
+        return isUserAuthenticated;
+    }
+
+    @Override
+    public void register(String username, String password) {
+        log.info("{}: Пользователь зарегистрирован: {}", this, username);
+    }
+}
