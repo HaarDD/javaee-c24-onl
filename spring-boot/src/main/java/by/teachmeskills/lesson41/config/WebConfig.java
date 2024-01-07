b2be2e78e265f42d7aa765a6b640d1e2675fbe39
HaarDD <maxgalitskiy@gmail.com>
2024-01-07 05:19:29 +0300

    feat: add interceptor switch & resource handler & request logging parameters

--- spring-boot/src/main/java/by/teachmeskills/lesson41/config/WebConfig.java
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/config/WebConfig.java
@@ -1,11 +1,13 @@
-package by.teachmeskills.lesson39.config;
+package by.teachmeskills.lesson41.config;
 
-import by.teachmeskills.lesson39.config.converters.StringToAuthorDtoConverter;
-import by.teachmeskills.lesson39.logging.LoggingInterceptor;
+import by.teachmeskills.lesson41.config.converters.StringToAuthorDtoConverter;
+import by.teachmeskills.lesson41.logging.LoggingInterceptor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -16,27 +18,20 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan("by.teachmeskills.lesson39")
+@ComponentScan("by.teachmeskills.lesson41")
 public class WebConfig implements WebMvcConfigurer {
-
-    @Bean
-    ViewResolver viewResolver() {
-        InternalResourceViewResolver internalResourceViewResolver = new InternalResourceViewResolver();
-        internalResourceViewResolver.setSuffix(".jsp");
-        internalResourceViewResolver.setPrefix("/WEB-INF/pages/");
-        internalResourceViewResolver.setContentType("text/html;charset=UTF-8");
-        return internalResourceViewResolver;
-    }
 
     @Bean
     HandlerInterceptor loggingInterceptor(){
         return new LoggingInterceptor();
     }
 
+    @Value("${logging.interceptor.enabled}")
+    private boolean isLoggingInterceptorEnabled;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/js/**").addResourceLocations("/js/");
-        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
     }
 
     @Override
@@ -46,7 +41,20 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // Отключено для чистых логов
-        //registry.addInterceptor(loggingInterceptor());
+        // Добавлен переключатель в application.yaml
+        if(isLoggingInterceptorEnabled){
+            registry.addInterceptor(loggingInterceptor());
+        }
+    }
+
+    // Логирование request`ов
+    @Bean
+    public CommonsRequestLoggingFilter requestLoggingFilter() {
+        CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+        loggingFilter.setIncludeClientInfo(true);
+        loggingFilter.setIncludeQueryString(true);
+        loggingFilter.setIncludePayload(true);
+        loggingFilter.setMaxPayloadLength(64000);
+        return loggingFilter;
     }
 }
