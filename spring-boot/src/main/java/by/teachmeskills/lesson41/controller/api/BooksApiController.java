abb7e4300ceece0e55308f190ab73b135ffdda93
HaarDD <maxgalitskiy@gmail.com>
2024-01-08 13:44:13 +0300

    fix: change to RestController

--- spring-boot/src/main/java/by/teachmeskills/lesson41/controller/api/BooksApiController.java
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/controller/api/BooksApiController.java
@@ -7,7 +7,7 @@ import by.teachmeskills.lesson41.exception.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -15,13 +15,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
 @Slf4j
-@Controller
+@RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/book")
 public class BooksApiController {
@@ -29,10 +29,10 @@ public class BooksApiController {
     private final BooksRepository booksRepository;
 
     @GetMapping
-    @ResponseBody
-    public BookDto getBookById(@RequestParam Long id) {
-        return booksRepository.getBookById(id)
+    public ResponseEntity<BookDto> getBookById(@RequestParam Long id) {
+        BookDto bookDto  = booksRepository.getBookById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Книга с id %s не найдена!".formatted(id)));
+        return ResponseEntity.ok(bookDto);
     }
 
     @PostMapping
