63149c457a5f7bf7a81c38f021d9cf5cb131e36a
HaarDD <maxgalitskiy@gmail.com>
2024-01-08 13:39:56 +0300

    feat: change sql queries to named parameters method

--- spring-boot/src/main/java/by/teachmeskills/lesson41/dao/AuthorsRepository.java
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/dao/AuthorsRepository.java
@@ -2,79 +2,64 @@ package by.teachmeskills.lesson41.dao;
 
 import by.teachmeskills.lesson41.dbservice.DatabaseManager;
 import by.teachmeskills.lesson41.dto.AuthorDto;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Repository
+@RequiredArgsConstructor
 public class AuthorsRepository {
-    private static final String INSERT_AUTHOR_SQL = "INSERT INTO author (name) VALUES (?)";
+    private static final String INSERT_AUTHOR_SQL = "INSERT INTO author (name) VALUES (:name)";
     private static final String SELECT_ALL_AUTHORS_SQL = "SELECT * FROM author";
-    private static final String DELETE_AUTHOR_SQL = "DELETE FROM author WHERE id = ?";
-    private static final String SELECT_AUTHOR_BY_NAME_SQL = "SELECT * FROM author WHERE name = ?";
-    private static final String SELECT_AUTHOR_BY_ID_SQL = "SELECT * FROM author WHERE id = ?";
-    private static final String UPDATE_AUTHOR_SQL = "UPDATE author SET name = ? WHERE id = ?";
-    private static final String SELECT_AUTHORS_BY_IDS_SQL = "SELECT * FROM author WHERE id IN ";
+    private static final String DELETE_AUTHOR_SQL = "DELETE FROM author WHERE id = :id";
+    private static final String SELECT_AUTHOR_BY_NAME_SQL = "SELECT * FROM author WHERE name = :name";
+    private static final String SELECT_AUTHOR_BY_ID_SQL = "SELECT * FROM author WHERE id = :id";
+    private static final String UPDATE_AUTHOR_SQL = "UPDATE author SET name = :name WHERE id = :id";
+    private static final String SELECT_AUTHORS_BY_IDS_SQL = "SELECT * FROM author WHERE id IN (:ids)";
 
     public List<AuthorDto> getAuthorsByIds(List<Long> authorsIds) {
         List<AuthorDto> authors = new ArrayList<>();
 
-        try (Connection connection = DatabaseManager.getConnection()) {
-            StringBuilder queryBuilder = new StringBuilder();
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("ids", authorsIds);
 
-            if (authorsIds == null || authorsIds.isEmpty()) {
-                queryBuilder.append(SELECT_ALL_AUTHORS_SQL);
-            } else {
-                queryBuilder.append(SELECT_AUTHORS_BY_IDS_SQL);
-                String idsString = authorsIds.stream().map(String::valueOf).collect(Collectors.joining(","));
-                queryBuilder.append("(").append(idsString).append(")");
-            }
-
-            try (PreparedStatement preparedStatement = connection.prepareStatement(queryBuilder.toString())) {
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                    AuthorDto author = new AuthorDto(
+            authors = jdbcTemplate.query(SELECT_AUTHORS_BY_IDS_SQL, parameters, (resultSet, rowNum) ->
+                    new AuthorDto(
                             resultSet.getLong("id"),
                             resultSet.getString("name")
-                    );
-                    authors.add(author);
-                }
-            }
-        } catch (SQLException e) {
+                    )
+            );
+            log.error("НЕ НАЙДЕНЫ: {}", Arrays.toString(authors.toArray()));
+        } catch (Exception e) {
             log.error("Ошибка получения списка авторов по идентификаторам: ", e);
         }
 
         return authors;
     }
 
-
     public List<AuthorDto> getAllAuthors() {
         List<AuthorDto> authors = new ArrayList<>();
 
-        try (Connection connection = DatabaseManager.getConnection()) {
-
-            try (PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_AUTHORS_SQL)) {
-                ResultSet resultSet = preparedStatement.executeQuery();
-                while (resultSet.next()) {
-                    AuthorDto author = new AuthorDto(
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            authors = jdbcTemplate.query(SELECT_ALL_AUTHORS_SQL, (resultSet, rowNum) ->
+                    new AuthorDto(
                             resultSet.getLong("id"),
                             resultSet.getString("name")
-                    );
-                    authors.add(author);
-                }
-            }
-        } catch (SQLException e) {
-            log.error("Ошибка получения списка авторов по идентификаторам: ", e);
+                    )
+            );
+        } catch (Exception e) {
+            log.error("Ошибка получения списка авторов: ", e);
         }
 
         return authors;
@@ -86,15 +71,15 @@ public class AuthorsRepository {
             return Optional.empty();
         }
 
-        try (Connection connection = DatabaseManager.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(INSERT_AUTHOR_SQL, Statement.RETURN_GENERATED_KEYS)) {
-            preparedStatement.setString(1, authorDto.getName());
-            preparedStatement.executeUpdate();
-            ResultSet resultSet = preparedStatement.getGeneratedKeys();
-            if (resultSet.next()) {
-                return Optional.of(authorDto.setId(resultSet.getLong(1)));
-            }
-        } catch (SQLException e) {
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("name", authorDto.getName());
+
+            jdbcTemplate.update(INSERT_AUTHOR_SQL, parameters);
+
+            return getAuthorByName(authorDto.getName());
+        } catch (Exception e) {
             log.error("Ошибка добавления автора: ", e);
         }
 
@@ -102,18 +87,20 @@ public class AuthorsRepository {
     }
 
     public Optional<AuthorDto> getAuthorByName(String name) {
-        try (Connection connection = DatabaseManager.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_AUTHOR_BY_NAME_SQL)) {
-            preparedStatement.setString(1, name);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    return Optional.of(new AuthorDto(
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("name", name);
+
+            List<AuthorDto> authors = jdbcTemplate.query(SELECT_AUTHOR_BY_NAME_SQL, parameters, (resultSet, rowNum) ->
+                    new AuthorDto(
                             resultSet.getLong("id"),
                             resultSet.getString("name")
-                    ));
-                }
-            }
-        } catch (SQLException e) {
+                    )
+            );
+
+            return authors.isEmpty() ? Optional.empty() : Optional.of(authors.get(0));
+        } catch (Exception e) {
             log.error("Ошибка проверки существования автора: ", e);
         }
         return Optional.empty();
@@ -125,30 +112,36 @@ public class AuthorsRepository {
             log.warn("Автора с id \"{}\" не существует", authorId);
             return Optional.empty();
         }
-        try (Connection connection = DatabaseManager.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_AUTHOR_SQL)) {
-            preparedStatement.setLong(1, authorId);
-            preparedStatement.executeUpdate();
-            return optionalAuthorDto;
-        } catch (SQLException e) {
+
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("id", authorId);
+
+            jdbcTemplate.update(DELETE_AUTHOR_SQL, parameters);
+        } catch (Exception e) {
             log.error("Ошибка удаления автора: ", e);
+            return Optional.empty();
         }
-        return Optional.empty();
+
+        return optionalAuthorDto;
     }
 
     public Optional<AuthorDto> getAuthorById(Long authorId) {
-        try (Connection connection = DatabaseManager.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_AUTHOR_BY_ID_SQL)) {
-            preparedStatement.setLong(1, authorId);
-            try (ResultSet resultSet = preparedStatement.executeQuery()) {
-                if (resultSet.next()) {
-                    return Optional.of(new AuthorDto(
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("id", authorId);
+
+            List<AuthorDto> authors = jdbcTemplate.query(SELECT_AUTHOR_BY_ID_SQL, parameters, (resultSet, rowNum) ->
+                    new AuthorDto(
                             resultSet.getLong("id"),
                             resultSet.getString("name")
-                    ));
-                }
-            }
-        } catch (SQLException e) {
+                    )
+            );
+
+            return authors.isEmpty() ? Optional.empty() : Optional.of(authors.get(0));
+        } catch (Exception e) {
             log.error("Ошибка получения автора по идентификатору: ", e);
         }
         return Optional.empty();
@@ -159,16 +152,19 @@ public class AuthorsRepository {
             log.warn("Автора с id \"{}\" не существует", authorDto.getId());
             return Optional.empty();
         }
-        try (Connection connection = DatabaseManager.getConnection();
-             PreparedStatement preparedStatement = connection.prepareStatement(UPDATE_AUTHOR_SQL)) {
-            preparedStatement.setString(1, authorDto.getName());
-            preparedStatement.setLong(2, authorDto.getId());
-            preparedStatement.executeUpdate();
-            return Optional.of(authorDto);
-        } catch (SQLException e) {
-            log.error("Ошибка редактирования автора: ", e);
-        }
-        return Optional.empty();
-    }
 
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("id", authorDto.getId());
+            parameters.addValue("name", authorDto.getName());
+
+            jdbcTemplate.update(UPDATE_AUTHOR_SQL, parameters);
+
+            return Optional.of(authorDto);
+        } catch (Exception e) {
+            log.error("Ошибка редактирования автора: ", e);
+            return Optional.empty();
+        }
+    }
 }
--- spring-boot/src/main/java/by/teachmeskills/lesson41/dao/BooksAuthorsRepository.java
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/dao/BooksAuthorsRepository.java
@@ -1,62 +1,69 @@
 package by.teachmeskills.lesson41.dao;
 
+import by.teachmeskills.lesson41.dbservice.DatabaseManager;
 import by.teachmeskills.lesson41.dto.AuthorDto;
 import by.teachmeskills.lesson41.dto.BookAuthorDto;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
+@AllArgsConstructor
 @Repository
 public class BooksAuthorsRepository {
-    private static final String INSERT_BOOK_AUTHOR_SQL = "INSERT INTO book_author (bookId, authorId) VALUES (?, ?)";
-    private static final String SELECT_AUTHORS_FOR_BOOK = "SELECT author.* FROM author JOIN book_author ON author.id = book_author.authorId WHERE book_author.bookId = ?";
-    private static final String DELETE_BOOK_AUTHOR_RELATIONSHIP_SQL = "DELETE FROM book_author WHERE bookId = ? AND authorId = ?";
 
-    public void addBookAuthorRelationship(Connection connection, BookAuthorDto bookAuthorDto) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_BOOK_AUTHOR_SQL)) {
-            preparedStatement.setLong(1, bookAuthorDto.getBookId());
-            preparedStatement.setLong(2, bookAuthorDto.getAuthorId());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+    private static final String INSERT_BOOK_AUTHOR_SQL = "INSERT INTO book_author (bookId, authorId) VALUES (:bookId, :authorId)";
+    private static final String SELECT_AUTHORS_FOR_BOOK = "SELECT author.* FROM author JOIN book_author ON author.id = book_author.authorId WHERE book_author.bookId = :bookId";
+    private static final String DELETE_BOOK_AUTHOR_RELATIONSHIP_SQL = "DELETE FROM book_author WHERE bookId = :bookId AND authorId = :authorId";
+
+    public void addBookAuthorRelationship(BookAuthorDto bookAuthorDto) {
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("bookId", bookAuthorDto.getBookId());
+            parameters.addValue("authorId", bookAuthorDto.getAuthorId());
+
+            jdbcTemplate.update(INSERT_BOOK_AUTHOR_SQL, parameters);
+        } catch (Exception e) {
             log.error("Ошибка добавления связи между книгой и автором: ", e);
         }
     }
 
-    public void removeBookAuthorRelationship(Connection connection, BookAuthorDto bookAuthorDto) {
-        try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_BOOK_AUTHOR_RELATIONSHIP_SQL)) {
-            preparedStatement.setLong(1, bookAuthorDto.getBookId());
-            preparedStatement.setLong(2, bookAuthorDto.getAuthorId());
-            preparedStatement.executeUpdate();
-        } catch (SQLException e) {
+    public void removeBookAuthorRelationship(BookAuthorDto bookAuthorDto) {
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("bookId", bookAuthorDto.getBookId());
+            parameters.addValue("authorId", bookAuthorDto.getAuthorId());
+
+            jdbcTemplate.update(DELETE_BOOK_AUTHOR_RELATIONSHIP_SQL, parameters);
+        } catch (Exception e) {
             log.error("Ошибка удаления связи между книгой и автором: ", e);
         }
     }
 
-    public List<AuthorDto> getAuthorsForBook(Connection connection, long bookId) {
+    public List<AuthorDto> getAuthorsForBook(long bookId) {
         List<AuthorDto> authors = new ArrayList<>();
 
-        try (PreparedStatement statement = connection.prepareStatement(SELECT_AUTHORS_FOR_BOOK)) {
-            statement.setLong(1, bookId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                while (resultSet.next()) {
-                    authors.add(new AuthorDto(
+        try {
+            NamedParameterJdbcTemplate jdbcTemplate = new NamedParameterJdbcTemplate(DatabaseManager.getDataSource());
+            MapSqlParameterSource parameters = new MapSqlParameterSource();
+            parameters.addValue("bookId", bookId);
+
+            authors = jdbcTemplate.query(SELECT_AUTHORS_FOR_BOOK, parameters, (resultSet, rowNum) ->
+                    new AuthorDto(
                             resultSet.getLong("id"),
                             resultSet.getString("name"))
-                    );
-                }
-            }
-        } catch (SQLException e) {
+            );
+        } catch (Exception e) {
             log.error("Ошибка при получении списка авторов для книги: ", e);
         }
 
         return authors;
     }
-
 }
