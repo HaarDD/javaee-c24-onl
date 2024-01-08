2f885cebdd66ee827999327fe3588a98a9c251a6
HaarDD <maxgalitskiy@gmail.com>
2024-01-08 13:41:37 +0300

    feat: add method for books filter

--- spring-boot/src/main/java/by/teachmeskills/lesson41/controller/mvc/BooksMvcController.java
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/controller/mvc/BooksMvcController.java
@@ -1,13 +1,18 @@
 package by.teachmeskills.lesson41.controller.mvc;
 
 
+import by.teachmeskills.lesson41.dao.AuthorsRepository;
 import by.teachmeskills.lesson41.dao.BooksRepository;
+import by.teachmeskills.lesson41.dto.BookDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Slf4j
 @Controller
@@ -16,9 +21,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BooksMvcController {
 
     private final BooksRepository booksRepository;
+
+    private final AuthorsRepository authorsRepository;
+
     @GetMapping
-    public String showBooks(Model model) {
-        model.addAttribute("books", booksRepository.getAllBooks());
+    public String showBooks(Model model, @RequestParam(required = false) String searchText,
+                            @RequestParam(required = false) String searchType,
+                            @RequestParam(required = false) List<Long> authorSelect,
+                            @RequestParam(required = false) Long pagesFrom,
+                            @RequestParam(required = false) Long pagesTo) {
+        //TODO переключать на другой запрос при пустых параметрах
+        List<BookDto> bookDtoList = booksRepository.getFilteredBooks(searchText,searchType,authorSelect,pagesFrom,pagesTo);
+
+        model.addAttribute("books", bookDtoList);
+        model.addAttribute("authors", authorsRepository.getAllAuthors());
+        model.addAttribute("searchText", searchText);
+        model.addAttribute("searchType", searchType);
+        model.addAttribute("authorSelect", authorSelect);
+        model.addAttribute("pagesFrom", pagesFrom);
+        model.addAttribute("pagesTo", pagesTo);
         return "books";
     }
 }
