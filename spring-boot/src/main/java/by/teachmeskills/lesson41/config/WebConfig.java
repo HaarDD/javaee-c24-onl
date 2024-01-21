2137f25893db6629bff611d608f5eb4568b5f42a
HaarDD <maxgalitskiy@gmail.com>
2024-01-22 01:09:26 +0300

    feat: remove formatters enabler (automatically)

--- spring-boot/src/main/java/by/teachmeskills/lesson41/config/WebConfig.java
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/config/WebConfig.java
@@ -1,29 +1,25 @@
 package by.teachmeskills.lesson41.config;
 
-import by.teachmeskills.lesson41.config.converters.StringToAuthorDtoConverter;
 import by.teachmeskills.lesson41.logging.LoggingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.FormatterRegistry;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan("by.teachmeskills.lesson41")
 public class WebConfig implements WebMvcConfigurer {
 
     @Bean
-    HandlerInterceptor loggingInterceptor(){
+    HandlerInterceptor loggingInterceptor() {
         return new LoggingInterceptor();
     }
+
 
     @Value("${logging.interceptor.enabled}")
     private boolean isLoggingInterceptorEnabled;
@@ -34,14 +30,8 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
-    public void addFormatters(FormatterRegistry registry) {
-        registry.addFormatter(new StringToAuthorDtoConverter());
-    }
-
-    @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        // Добавлен переключатель в application.yaml
-        if(isLoggingInterceptorEnabled){
+        if (isLoggingInterceptorEnabled) {
             registry.addInterceptor(loggingInterceptor());
         }
     }
