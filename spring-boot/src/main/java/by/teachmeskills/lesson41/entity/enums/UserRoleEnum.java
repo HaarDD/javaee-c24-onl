dc5b2dcaa30c068fc1feda49d200cfa5b619e250
HaarDD <maxgalitskiy@gmail.com>
2024-02-14 18:57:32 +0300

    feat: user roles & statuses

---
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/entity/enums/UserRoleEnum.java
@@ -0,0 +1,5 @@
+package by.teachmeskills.lesson41.entity.enums;
+
+public enum UserRoleEnum {
+    GUEST, READER, LIBRARIAN, MANAGER
+}
---
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/security/UserStatus.java
@@ -0,0 +1,5 @@
+package by.teachmeskills.lesson41.security;
+
+public enum UserStatus {
+    OK, BLOCKED, LOGIN_EXPIRED, PASSWORD_EXPIRED
+}
