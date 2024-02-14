3ef23a98d151825df4c81831eb26ad205e803857
HaarDD <maxgalitskiy@gmail.com>
2024-02-14 19:00:27 +0300

    feat: security config

---
+++ spring-boot/src/main/java/by/teachmeskills/lesson41/config/SecurityConfiguration.java
@@ -0,0 +1,112 @@
+package by.teachmeskills.lesson41.config;
+
+import by.teachmeskills.lesson41.entity.enums.UserRoleEnum;
+import by.teachmeskills.lesson41.security.JwtFilter;
+import by.teachmeskills.lesson41.security.service.AuthenticationHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AnonymousAuthenticationFilter;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static by.teachmeskills.lesson41.controller.auth.AuthController.AUTH_REQUEST_MAPPING;
+import static by.teachmeskills.lesson41.security.UserAuthorities.LIBRARIAN;
+import static by.teachmeskills.lesson41.security.UserAuthorities.MANAGER;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    public static final String[] SWAGGER_AUTH_WHITELIST = new String[]{"/docs.html","/swagger-ui*/**",
+            "/swagger-ui*/*swagger-initializer.js", "/swagger-ui.html", "/webjars/**",
+            "/v3/api-docs**", "/bus/v3/api-docs**", "/api/docs.yaml/**"};
+
+    private final UserDetailsService userDetailsService;
+
+    private final AuthenticationHandler authenticationHandler;
+
+    private final JwtFilter jwtFilter;
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(SWAGGER_AUTH_WHITELIST);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                //.exceptionHandling().authenticationEntryPoint(new Http403ForbiddenEntryPoint())
+                //.and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .authorizeRequests()
+                .antMatchers(SWAGGER_AUTH_WHITELIST).permitAll()
+                .antMatchers("/", AUTH_REQUEST_MAPPING).permitAll()
+                .antMatchers("/api/users/reg").permitAll()
+                .antMatchers("/api/users/*").hasAnyAuthority(MANAGER, LIBRARIAN)
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/**").permitAll()
+                //.anyRequest().authenticated()
+                .and()
+                .addFilterAfter(jwtFilter, AnonymousAuthenticationFilter.class)
+                .logout();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public List<UserRoleEnum> userRights() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority)
+                .map(UserRoleEnum::valueOf)
+                .collect(Collectors.toList());
+    }
+
+    @Bean
+    @Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public UsernamePasswordAuthenticationToken user() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (UsernamePasswordAuthenticationToken) authentication;
+    }
+
+
+}
